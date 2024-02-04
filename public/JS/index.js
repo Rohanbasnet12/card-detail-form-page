fbaa16b30d98d58fd10b5d74c3834460bb79365d
Rohanbasnet12 <rohanbmyname@gmail.com>
2024-02-04 01:51:53 -0800

    Project not completed

--- public/JS/index.js
+++ public/JS/index.js
@@ -11,6 +11,8 @@ confirmButton.on('click', () => {
         $('.error-message-short').show();
         confirmButton.addClass('errorAnimation');
     }
+
+
 });
 
 
@@ -20,6 +22,9 @@ $('#card-number').on('input', function () {
     inputValue = inputValue.replace(/\s/g, ''); // Remove existing spaces
     let formattedValue = formatCreditCardNumber(inputValue);
     $(this).val(formattedValue);
+
+    // Update the content of an element with the id 'card-number-detail'
+    $('#card-number-detail').text(formattedValue);
 });
 
 function formatCreditCardNumber(inputValue) {
@@ -27,3 +32,33 @@ function formatCreditCardNumber(inputValue) {
     formattedValue = formattedValue.replace(/(\d{4})/g, '$1 ').trim(); // Add space after every 4 digits
     return formattedValue;
 }
+
+
+// Function to change the card holder name on the card detail
+$('#cardHolder-name').on('input', function () {
+    let input = $(this).val();
+    $('#cardholder-name-detail').text(input);
+});
+
+$('#month-detail').on('input', function () {
+    let monthInput = $(this).val();
+    if (monthInput > 31) {
+        $('.error-message-invalid').show();
+    }
+});
+
+$('#year-detail').on('input', function () {
+    let yearInput = $(this).val();
+    if (yearInput > 12) {
+        $('.error-message-invalid').show();
+    } else if (yearInput === 0) {
+        $('.error-message-short').show();
+    }
+});
+
+$('#cvc-number').on('input', function () {
+    let cvc = $(this).val();
+    $('.show-cvc-detail').text(cvc);
+    confirmButton.addClass('errorAnimation');
+});
+
